039ca5ac0cd6020c8e0eaee2f5548142ba9995d1
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-01-01 22:14:36 -0300

    Estado e dados dinâmicos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from "react";
+import { livros } from "./dadosPesquisa.js";
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -24,17 +25,47 @@ const Subtitulo = styled.h3`
   margin-bottom: 40px;
 `;
 
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
 function Pesquisa() {
-    const [textoDigitado, setTextoDigitado]  = useState('')
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
 
   return (
     <PesquisaContainer>
       <Titulo>Já sabe por onde começar?</Titulo>
       <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-      <Input placeholder="Escreva sua próxima leitura" onBlur={(evento) => setTextoDigitado(evento.target.value)}/>
-      <p>{textoDigitado}</p>
+      <Input
+        placeholder="Escreva sua próxima leitura"
+        onBlur={(evento) => {
+          const textoDigitado = evento.target.value;
+          const resultadoPesquisa = livros.filter((livro) =>
+            livro.nome.includes(textoDigitado)
+          );
+          setLivrosPesquisados(resultadoPesquisa);
+        }}
+      />
+      {livrosPesquisados.map((livro) => (
+        <Resultado>
+          <p>{livro.nome}</p>
+          <img src={livro.src} alt="" />
+        </Resultado>
+      ))}
     </PesquisaContainer>
-
   );
 }
 
