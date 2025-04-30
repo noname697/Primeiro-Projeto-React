bd25ec434d46ff4002d591f2b751bed2ab058a14
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-04-30 19:36:01 -0300

    <Link>

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
+const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "ESTANTE"];
 
 const Opcoes = styled.ul`
   display: flex;
@@ -22,9 +23,11 @@ function OpcoesHeader() {
   return (
     <Opcoes>
       {textoOpcoes.map((texto) => (
-        <Opcao>
-          <p>{texto}</p>
-        </Opcao>
+        <Link to={`/${texto.toLowerCase()}`}>
+          <Opcao>
+            <p>{texto}</p>
+          </Opcao>
+        </Link>
       ))}
     </Opcoes>
   );
